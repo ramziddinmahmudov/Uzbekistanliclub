df8f0e86b328bf0fffcc579298269ef1ee13d40c
Mahmudov Ramziddin <mahmudovramziddin25@gmail.com>
2024-08-06 13:45:06 +0500

    Last update

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -5,7 +5,7 @@ function Top6() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    fetch('/public/list.json')
+    fetch('https://tfeemydipxlrphrakfuo.supabase.co/storage/v1/object/sign/gaery/list.json?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJnYWVyeS9saXN0Lmpzb24iLCJpYXQiOjE3MjI5MzM4NTksImV4cCI6MTc1NDQ2OTg1OX0.LpvZjUiyjK6BjSCyAX7AnfKOrWAV6vGkXKGldqZiIoY&t=2024-08-06T08%3A44%3A16.251Z')
       .then((response) => response.json())
       .then((data) => {
         setPosts(data.profile);
